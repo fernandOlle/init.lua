08d2e829c43be1f193b90277001b08b7d46709b0
fernandOlle <fernando.ribeiro.olle@gmail.com>
2022-12-21 21:32:31 -0300

    replace profile name

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -112,7 +112,7 @@ _G.packer_plugins = {
   harpoon = {
     loaded = true,
     path = "/home/mpaulson/.local/share/nvim/site/pack/packer/start/harpoon",
-    url = "https://github.com/theprimeagen/harpoon"
+    url = "https://github.com/fernandolle/harpoon"
   },
   ["lsp-zero.nvim"] = {
     loaded = true,
